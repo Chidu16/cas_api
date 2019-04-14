3fd01f496976c18772305ea2850a19212f963b24
Chidambar Gokak <x18104291@student.ncirl.ie>
2019-04-14 20:47:37 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    Review.create(name: 'Jack Ma', comment: 'Blockchain technology could change our world more than people imagine, Bitcoin however could be a bubble.')
+    Review.create(name: 'Eric Schmidt', comment: 'Bitcoin is a remarkable cryptographic achievement and the ability to create something that is not duplicable in the digital world has enormous value.')
+    Review.create(name: 'Ben Bernanke', comment: 'Virtual Currencies may hold long-term promise, particularly if the innovations promote a faster, more secure and more efficient payment system.')
+    Review.create(name: 'Chris Dixon', comment: 'There are three eras of currency: Commodity based, politically based, and now, math based.')
+    Review.create(name: 'Al Gore', comment: 'I think the fact that within the Bitcoin universe an algorithm replaces the functions of the government is actually pretty cool. I am a big fan of Bitcoin.')
+    Review.create(name: 'Bill Gates', comment: 'Bitcoin is a technological tour de force.')
+    Review.create(name: 'Richard Branson', comment: 'Well, I think it is working. There may be other currencies like it that may be even better. But in the meantime, there’s a big industry around Bitcoin. — People have made fortunes off Bitcoin, some have lost money. It is volatile, but people make money off of volatility too.')
+    Review.create(name: 'Gavin Andresen', comment: 'Just as it got easier to use email, it will be easier to use Bitcoin as people invest in it and become more familiar with it')
+    Review.create(name: 'Charlie Lee', comment: 'There will be at most 21 million Bitcoins in existence. There isn’t even enough BTC to go around for EVERY millionaire to own one. So before you buy any other coin (LTC included), try to own at least 1 BTC first.')
+    Review.create(name: 'Warren Buffett', comment: 'You can’t value Bitcoin because it’s not a value-producing asset.')
+    Review.create(name: 'Jamie Dimon', comment: 'The currency isn’t going to work. You can’t have a business where people can invent a currency out of thin air and think that people who are buying it are really smart.')
+    Review.create(name: 'Peter Thiel', comment: 'PayPal had these goals of creating a new currency. We failed at that, and we just created a new payment system. I think Bitcoin has succeeded on the level of a new currency.')
+    
+    
